d7badd47c0e7931475f15a445547885c8f056df6
FiniYang <fini.yang@gmail.com>
2017-01-13 21:51:32 +0800

    remove Home About mock data

--- src/views/Home/page.js
+++ src/views/Home/page.js
@@ -61,7 +61,7 @@ class Home extends Component {
 
 
   render() {
-    let projItems = this.props.projList.map((item,index) => <ProjectItem projInfo={item} key={index} />)
+    let projItems = this.props.projList.length>0 ? this.props.projList.map((item,index) => <ProjectItem projInfo={item} key={index} />) : ""
     return (
       <div>
         <div styleName="top-container">
