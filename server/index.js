61b2d2fa01387b0ca04546ad7f3052a2089e7d64
FiniYang <fini.yang@gmail.com>
2017-08-11 16:59:46 +0800

    fix bug

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import webpack from 'webpack'
 import historyApiFallback from 'koa-connect-history-api-fallback'
 import serve from 'koa-static'
 import webpackConfig from '../config/_basic_webpack'
-import proxy from 'koa-proxy';
 
 const config = require('../config')
 const paths = config.utils_paths
@@ -20,23 +19,6 @@ app.use(convert(historyApiFallback({
     verbose: false
 })))
 
-// mock
-// app.use(proxy({
-//   host: 'http://192.168.132.44:8081',
-//   match: /^\/api\//,
-//   map: path => /^\/api\//.test(path) ? path.replace(/^\/api\//, '') : path
-// }));
-
-// node
-// app.use(proxy({
-//   host: 'http://192.168.132.43',
-//   match: /^\/api\//,
-// }));
-
-// app.use(proxy({
-//   host: 'http://127.0.0.1:80',
-//   match: /^\/api\//
-// }));
 // ------------------------------------
 // Apply Webpack HMR Middleware
 // ------------------------------------
