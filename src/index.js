e4cd3d6c53d428b81a880dc36395d002f49139f8
sean <sean@tuhuadmindeMacBook-Pro-2.local>
2018-03-30 09:59:17 +0800

    git adding master 3333

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 // Android 4.4 Version Below
 // ------------------------------------
 require('es6-promise').polyfill();
-var a = 333;
+var a = 3333;
 
 // ------------------------------------
 // Resource Import
@@ -50,7 +50,7 @@ require.ensure([],
         // Debug Here
         // ------------------------------------
         html5ForLogin(html5ForStartApp);
-        var b = 333;
+        var b = 3333 ;
 
         // ------------------------------------
         // Client Login
