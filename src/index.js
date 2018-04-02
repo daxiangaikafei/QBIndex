4368f8a4302f4767fd2b78bf17f54c00944bced5
sean <sean@tuhuadmindeMacBook-Pro-2.local>
2018-04-02 18:02:16 +0800

    adding withoutNode 123

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 // Android 4.4 Version Below
 // ------------------------------------
 require('es6-promise').polyfill();
-var a = 12;
+var a = 123;
 
 // ------------------------------------
 // Resource Import
@@ -50,7 +50,7 @@ require.ensure([],
         // Debug Here
         // ------------------------------------
         html5ForLogin(html5ForStartApp);
-        var b = 12;
+        var b = 123;
 
         // ------------------------------------
         // Client Login
