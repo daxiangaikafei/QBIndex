59c09c63fd34741a64aad561cc6cda2fff85ddf6
sean <sean@tuhuadmindeMacBook-Pro-2.local>
2018-04-02 17:53:38 +0800

    adding master 00

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 // Android 4.4 Version Below
 // ------------------------------------
 require('es6-promise').polyfill();
-var a = 0;
+var a = 00;
 
 // ------------------------------------
 // Resource Import
@@ -50,7 +50,7 @@ require.ensure([],
         // Debug Here
         // ------------------------------------
         html5ForLogin(html5ForStartApp);
-        var b = 0;
+        var b = 00;
 
         // ------------------------------------
         // Client Login
